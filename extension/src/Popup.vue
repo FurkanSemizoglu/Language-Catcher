b339015b1fb0ffa9ffd08a4cbcd5fddf559d1d32
Furkan <furkan5590@hotmail.com>
2024-07-09 11:51:15 +0300

    main view template was completed

--- extension/src/Popup.vue
+++ extension/src/Popup.vue
@@ -1,5 +1,34 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue';
+
+
+const showUrl = ref<boolean>(false);
+const showLanguage = ref<boolean>(false);
+
+const showUrlButtonClicked = ()=>{
+  showUrl.value = !showUrl.value;
+}
+
+const showLanguageButtonClicked = ()=>{
+  showLanguage.value = !showLanguage.value;
+}
+
+console.log(showUrl.value);
+</script>
 
 <template>
-  <h1 class="m-1 text-red w-xl">Chrome Extension Project with VUE</h1>
+  <div class="min-w-md max-w-md flex flex-col items-center py-2 gap-1 ">
+    <div v-if="showUrl" class="flex ">
+      <input class="w-270px p-2" type="text" placeholder="Type a url to detect the language"/>
+      <button>Search</button>
+    </div>
+
+    <div class="flex gap-1">
+      <button class="p-2 bg-#0059f7 border rounded-lg text-white text-xl cursor-pointer shadow-xl" @click="showLanguageButtonClicked">Show The Language</button>
+
+      <button  class="p-2 bg-#0059f7 border rounded-lg text-white text-xl cursor-pointer" @click="showUrlButtonClicked" >Or Write An Url</button>
+    </div>
+
+    <span v-if="showLanguage">Türkçe</span>
+  </div>
 </template>
