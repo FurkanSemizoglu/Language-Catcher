2bd64fad94359fffc2ca1ac1936257d34ec1357e
Furkan <furkan5590@hotmail.com>
2024-07-09 13:25:40 +0300

    lang detection functions are ready for development

--- extension/src/background.ts
+++ extension/src/background.ts
@@ -1,33 +1,30 @@
 console.log('background is running')
 
-/* chrome.browserAction.onClicked.addListener(function (tab) {
-    chrome.tabs.create(
-        {
-            url: chrome.extension.getURL('./popup/popup.html'),
-        },
-        function (tab) { }
-    );
-}); */
 
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log('background received message', request)
-    console.log("URL : " , request.url);
+  console.log('background received message', request)
+  console.log('URL : ', request.url)
 
-    if(request.message === 'URL-sended') {
-        console.log('URL-sended')
-        const newURL : string = request.url  
-      
+  if (request.message === 'URL-sended') {
+    console.log('URL-sended')
+    const newURL: string = request.url
 
-        try {
-            console.log("trying new tab creation");
-            chrome.tabs.create({ url: newURL });
-            console.log("tab created successfully");
-        } catch (error) {
-            console.log("error while creating new tab", error);
-        }
-        
+    try {
+      console.log('trying new tab creation')
+      chrome.tabs.create({ url: newURL })
+      console.log('tab created successfully')
+
+      setTimeout(() => {
+        console.log("message from background to content");
+        chrome.tabs.query({ active: true, currentWindow: true }, (tabs : any) => {
+          chrome.tabs.sendMessage(tabs[0].id, { action : 'ready-to-detect' }, (response : any) =>  {})
+        })
+      }, 1000)
+    } catch (error) {
+      console.log('error while creating new tab', error)
     }
-    sendResponse('background received message')
-    return true
+  }
+  sendResponse('background received message')
+  return true
 })
--- extension/src/content.ts
+++ extension/src/content.ts
@@ -1,1 +1,35 @@
 console.log('content is running')
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+
+    console.log("content received message", request);
+  if (request.action === 'ready-to-detect') {
+    console.log('ready-to-detect')
+
+    setTimeout(() => {
+        console.log("contetn");
+    }, 3000)
+  }
+})
+
+const parseURL = (url: string) => {
+  // parse Url to find language
+  // maybe it can be return a string
+
+    const parsedUrl : string[] = url.split('/') 
+
+}
+
+const detectMetaTag = () => {
+  // Finds the meta tag
+}
+
+const detectHtmlLang = () => {
+  // Finds the html lang attribute
+}
+
+const takeParagraphs = () => {}
+
+const detectLangFromStorage = () => {
+  // Finds the language from storage
+}
