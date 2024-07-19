6382ac5274914b69f2856b84f0efd7fc61acb820
Furkan <furkan5590@hotmail.com>
2024-07-19 12:25:31 +0300

    Add .env to .gitignore

--- application/server/controllers/test.ts
+++ application/server/controllers/test.ts
@@ -2,7 +2,7 @@ const express = require('express');
 import { Request, Response } from 'express';
 
 const test = (req: Request, res: Response) => {
-  res.json("Hello world!, everything is looking fine^^");
+  res.json("Hello world!, everything isss looking fine^^");
 };
 
 module.exports = {
