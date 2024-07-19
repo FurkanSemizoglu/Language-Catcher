f30da079d95a55664f673795b6b2bc54fd70ae8f
Furkan <furkan5590@hotmail.com>
2024-07-19 12:01:13 +0300

    Added UnoCSS for styling

--- application/client/vite.config.ts
+++ application/client/vite.config.ts
@@ -1,12 +1,13 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
+import UnoCSS from 'unocss/vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    vue(),UnoCSS(),
   ],
   resolve: {
     alias: {
